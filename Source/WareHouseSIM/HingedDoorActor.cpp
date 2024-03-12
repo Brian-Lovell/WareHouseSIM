49b12f44d87fa5cf1d354ba422fa64988b2c1491
Brian Lovell <brian.lovell@outlook.com>
2024-03-12 16:07:58 -0400

    [Init] hinged door code

---
+++ Source/WareHouseSIM/HingedDoorActor.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HingedDoorActor.h"
+
+// Sets default values
+AHingedDoorActor::AHingedDoorActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create Default Components
+	DoorFrame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorFrameMesh"));
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+	DoorComponent = CreateDefaultSubobject<UHingedDoorComponent>(TEXT("Hinged Door Component"));
+	
+	// Setup Attachments
+	DoorFrame->SetupAttachment(RootComponent);
+	Door->AttachToComponent(DoorFrame,FAttachmentTransformRules::KeepRelativeTransform);
+
+}
+
+// Called when the game starts or when spawned
+void AHingedDoorActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHingedDoorActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/WareHouseSIM/HingedDoorActor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HingedDoorComponent.h"
+#include "GameFramework/Actor.h"
+#include "HingedDoorActor.generated.h"
+
+UCLASS()
+class WAREHOUSESIM_API AHingedDoorActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHingedDoorActor();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* DoorFrame;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Door;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UHingedDoorComponent* DoorComponent;
+	
+
+};
